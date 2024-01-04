0cdf0a4093933862eca568c0dd63ee41f063dc21
kevin ocansey <kevinocansey11@gmail.com>
2024-01-04 18:17:11 +0000

    doc: updated indexjs. I added an event listener which fetches an image when get kitten button is clicked

--- public/index.js
+++ public/index.js
@@ -1,1 +1,25 @@
-// Your code here
+document.addEventListener("DOMContentLoaded", () => {
+  const url = "https://api.thecatapi.com/v1/images/search";
+  const fetchKittenButton = document.getElementById("fetchKittenButton");
+  const kittenImageContainer = document.getElementById("kittenImageContainer");
+  fetchKittenButton.addEventListener("click", getRandomCats);
+
+  randomCatPhoto = (json) => {
+    let photo = json[0].url;
+
+    console.log(json);
+    kittenImageContainer.classList.add("cats");
+    let image = document.createElement("img");
+    image.src = photo;
+    kittenImageContainer.append(image);
+  };
+
+  async function getRandomCats() {
+    kittenImageContainer.innerHTML = "";
+    try {
+      const response = await fetch(url);
+      const json = await response.json();
+      return randomCatPhoto(json);
+    } catch {}
+  }
+});
